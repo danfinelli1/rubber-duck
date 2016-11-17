b4dc92c11b0af5ee312fc151973f6e0b90109631
Dan Finelli <dan.finelli@hotmail.com>
2016-11-16 16:08:07 -0800

    very basic set up

---
+++ server.js
@@ -0,0 +1,35 @@
+//require express in our app
+var express = require('express'),
+  bodyParser = require('body-parser');
+
+// generate a new express app and call it 'app'
+var app = express();
+
+// serve static files in public
+app.use(express.static('public'));
+
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var controllers = require('./controllers');
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
